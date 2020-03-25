e98729747172057e92e68a6f7d71e8c9d5450d0a
Petter Suul <petter@suul.no>
2020-03-25 16:28:31 +0100

    Fix locale in query

--- web/views.py
+++ web/views.py
@@ -16,5 +16,5 @@ def index(request):
     locale = checklocale(request.get_host()),
     return render(request, 'index.html', {
         'page': client.entries({'content_type': 'page', 'fields.slug': 'index', 'locale': locale})[0],
-        'nav': client.entries({'content_type': 'menu', 'locale': 'nb-NO'})
+        'nav': client.entries({'content_type': 'menu', 'locale': locale})
     })
